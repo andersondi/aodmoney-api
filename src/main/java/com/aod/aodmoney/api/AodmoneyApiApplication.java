7351b80ba5b2a574271f4e214efa8805a22ce5ce
Anderson Dias <oliveiradiasa@gmail.com>
2021-01-14 14:58:29 -0300

    first commit

---
+++ src/main/java/com/aod/aodmoney/api/AodmoneyApiApplication.java
@@ -0,0 +1,13 @@
+package com.aod.aodmoney.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AodmoneyApiApplication {
+
+  public static void main( String[] args ) {
+    SpringApplication.run( AodmoneyApiApplication.class, args );
+  }
+
+}
---
+++ src/test/java/com/aod/aodmoney/api/AodmoneyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aod.aodmoney.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AodmoneyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
